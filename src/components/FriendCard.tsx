52b8fc09136d5ee438fdac4b43f7156a686695bb
Jiahua Chen <33486899+jchen@users.noreply.github.com>
2023-01-29 05:13:33 -0500

    Friend card redirects to friend

--- src/components/FriendCard.tsx
+++ src/components/FriendCard.tsx
@@ -9,16 +9,18 @@ interface FriendCardProps {
 
 export default function FriendCard(props: FriendCardProps) {
   return (
-    <Card compact className="w-full">
-      <Card.Body>
-        <div className="flex items-center gap-8">
-          <div className="flex grow flex-col gap-1">
-            <Card.Title>{props.friend.name}</Card.Title>
-            <FriendlyRange />
+    <a href={`/friend/${props.friend.id}`}>
+      <Card compact className="w-full">
+        <Card.Body>
+          <div className="flex items-center gap-8">
+            <div className="flex grow flex-col gap-1">
+              <Card.Title>{props.friend.name}</Card.Title>
+              <FriendlyRange />
+            </div>
+            <ChevronRightIcon className="h-6 w-6" />
           </div>
-          <ChevronRightIcon className="h-6 w-6" />
-        </div>
-      </Card.Body>
-    </Card>
+        </Card.Body>
+      </Card>
+    </a>
   );
 }
