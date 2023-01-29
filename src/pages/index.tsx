763d1d15db543324ab014093cdbead849a67f6ac
Jiahua Chen <33486899+jchen@users.noreply.github.com>
2023-01-28 20:51:41 -0500

    defaults to sign in with google

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -42,9 +42,11 @@ const AuthShowcase: React.FC = () => {
       </p>
       <button
         className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
-        onClick={sessionData ? () => void signOut() : () => void signIn()}
+        onClick={
+          sessionData ? () => void signOut() : () => void signIn("google")
+        }
       >
-        {sessionData ? "Sign out" : "Sign in"}
+        {sessionData ? "Sign out" : "Sign in with Google"}
       </button>
     </div>
   );
