f2711a69fb6b5b0d61e6262e74121ea8d9884558
Jiahua Chen <33486899+jchen@users.noreply.github.com>
2023-01-29 10:58:25 -0500

    meetr end meets

--- src/components/MeetsPage.tsx
+++ src/components/MeetsPage.tsx
@@ -37,6 +37,15 @@ export default function MeetsList() {
                 </div>
               ))}
         </ul>
+        <div className="flex justify-center opacity-40">
+          <h1 className="select-none text-4xl font-black">
+            <span className="text-[#1459C1]">m</span>
+            <span className="text-[#7AB8F1]">e</span>
+            <span className="text-[#E9DD74]">e</span>
+            <span className="text-[#E1814A]">t</span>
+            <span className="text-[#CF2222]">r</span>
+          </h1>
+        </div>
       </div>
     </div>
   );
