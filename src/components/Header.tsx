52726ad3fd633a35633095d27b8d16aefa406ec2
Jiahua Chen <33486899+jchen@users.noreply.github.com>
2023-01-29 06:51:24 -0500

    add link to home page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,18 @@
+import Link from "next/link";
 import HeaderTabs from "./HeaderTabs";
 
 export default function Header() {
   return (
     <header className="sticky top-0 z-50 flex h-16 w-full items-center justify-between bg-white px-4 drop-shadow-md md:px-6">
-      <h1 className="select-none text-4xl font-black">
-        <span className="text-[#1459C1]">m</span>
-        <span className="text-[#7AB8F1]">e</span>
-        <span className="text-[#E9DD74]">e</span>
-        <span className="text-[#E1814A]">t</span>
-        <span className="text-[#CF2222]">r</span>
-      </h1>
+      <Link href="/">
+        <h1 className="select-none text-4xl font-black">
+          <span className="text-[#1459C1]">m</span>
+          <span className="text-[#7AB8F1]">e</span>
+          <span className="text-[#E9DD74]">e</span>
+          <span className="text-[#E1814A]">t</span>
+          <span className="text-[#CF2222]">r</span>
+        </h1>
+      </Link>
       <HeaderTabs />
     </header>
   );
