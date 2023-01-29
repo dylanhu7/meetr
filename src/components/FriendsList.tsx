1a28c528f46caa1242c0865dc574f9e9aee04c16
Jiahua Chen <33486899+jchen@users.noreply.github.com>
2023-01-29 07:18:36 -0500

    Counts the number of meetings tracked

--- src/components/FriendsList.tsx
+++ src/components/FriendsList.tsx
@@ -63,8 +63,12 @@ export default function FriendsList() {
           </Stats.Stat>
           <Stats.Stat>
             <Stats.Stat.Item variant="title">Meets</Stats.Stat.Item>
-            {/* TODO: number of meets */}
-            <Stats.Stat.Item variant="value">325</Stats.Stat.Item>
+            <Stats.Stat.Item variant="value">
+              {friends.data?.reduce(
+                (acc, friend) => acc + friend.events.length,
+                0
+              )}
+            </Stats.Stat.Item>
             <Stats.Stat.Item variant="desc">
               tracked with <b>meetr</b>
             </Stats.Stat.Item>
